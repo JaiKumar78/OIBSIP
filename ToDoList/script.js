90a5534f3082c8670bdfe252f4cc321f971a6fda
Jai Kumar D <133518055+JaiKumar78@users.noreply.github.com>
2023-07-03 22:28:12 +0530

    Add files via upload

---
+++ ToDoList/script.js
@@ -0,0 +1,53 @@
+const todoList = new Array;
+
+function mytodo(){
+    const taskInput = document.querySelector('.todo');
+    const task =taskInput.value;
+
+    const dateInput = document.querySelector('.deadline');
+    const date = dateInput.value;
+
+    todoList.push({
+        taskName: task,
+        dueDate: date
+    });
+
+    displayTodo();
+    taskInput.value = '';
+    dateInput.value = '';
+}
+
+function displayTodo(){
+    let todoListHTML = '';
+    for(let i = 0; i < todoList.length; i++){
+        const todoDetails = todoList[i];
+        const taskName = todoDetails.taskName;
+        const dueDate = todoDetails.dueDate;
+
+        const todoHTML = `
+        <p class="taskName">${taskName}</p>
+        <p class="dueDate">${dueDate}</p>
+        <button class="deleteButton" onclick="removetodo(${i})">Remove</button>`;
+
+        todoListHTML += todoHTML;
+    }
+
+    document.querySelector('.listoftodos').innerHTML = todoListHTML;
+}
+
+function removetodo(index){
+    todoList.splice(index, 1);
+    displayTodo();
+}
+
+function refresh() {
+    let todoListHTML = '';
+    document.querySelector('.listoftodos').innerHTML = todoListHTML;
+}
+
+function priority(){
+    todoList.sort(function (a, b){
+        return new Date(a.dueDate) - new Date(b.dueDate);
+    });
+    displayTodo();
+}
